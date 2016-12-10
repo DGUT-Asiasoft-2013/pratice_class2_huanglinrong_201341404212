bcc0b4ba2d7f7bc251d64be1f79c20a19ffad60f
fourpeople <huanglinrong1994@qq.com>
2016-12-10 10:35:10 +0800

    实现了账号密码登陆验证

--- myapplication4/src/main/java/com/example/myapplication/LoginActivity.java
+++ myapplication4/src/main/java/com/example/myapplication/LoginActivity.java
@@ -1,12 +1,25 @@
 package com.example.myapplication;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.myapplication.fragment.inputcells.SimpleTextInputCellFragment;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 /**
  * Created by Administrator on 2016/12/3.
@@ -45,7 +58,7 @@ public class LoginActivity extends Activity {
         });
     }
 
-    void startForgotPassword(){
+    void startForgotPassword() {
         Intent intent = new Intent(this, PasswordRecoverActivity.class);
         startActivity(intent);
     }
@@ -55,16 +68,111 @@ public class LoginActivity extends Activity {
         startActivity(intent);
         //finish();
     }
+
     void startLoginActivity() {
+        //把账号密码post到服务器
+        String account = fragmentUsername.getText();
+        String passwordHash = fragmentPassword.getText();
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("account", account)
+                .addFormDataPart("passwordHash", passwordHash)
+                .build();
+        Request requestLogin = new Request.Builder()
+                .url("http://172.27.0.33:8080/membercenter/api/login")
+                .method("post", null)
+                .post(requestBody)//post上去的内容
+                .build();
+
+        //登陆中的提示信息的显示
+        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this);
+        progressDialog.setMessage("Login...");//设置内容
+        progressDialog.setCancelable(false);//设置不能点击取消
+        progressDialog.setCanceledOnTouchOutside(false);//设置一直在显示前面
+        progressDialog.show();//设置显示
+
+        okHttpClient.newCall(requestLogin).enqueue(new Callback() {
+            @Override
+            public void onFailure(final Call call, final IOException e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressDialog.dismiss();
+                        onFailure(call, e);
+                    }
+                });
+
+            }
+
+            @Override
+            public void onResponse(final Call call, final Response response) throws IOException {
+                final String message = response.body().string();//把返回的2response转换为String,用于下面判断
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressDialog.dismiss();//登陆中提示信息取消显示
+                        if (message.equals("")) {
+                            LoginActivity.this.onFailure(call, "密码错误！");//密码错误跳转到失败提示方法
+                        } else {
+                            try {
+                                LoginActivity.this.onResponse(call,message);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                                LoginActivity.this.onFailure(call, e);//有问题跳转到失败提示方法
+                            }
+                        }
+                    }
+                });
+            }
+        });
+
+
+    }
+
+    //请求成功时显示登陆信息和跳转页面
+    void onResponse(Call call, String response) {
+        new AlertDialog.Builder(this)
+                .setTitle("请求成功")
+                .setMessage(response)//response是返回的登陆信息
+                .setPositiveButton("确认", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //finish();
+                    }
+                })
+                .show();
         Intent intent = new Intent(this, HelloWorldActivity.class);
         startActivity(intent);
         //finish();
     }
 
+    //请求失败时显示的提示
+    void onFailure(Call call, Exception e) {
+        new AlertDialog.Builder(this)
+                .setTitle("请求失败")
+                .setMessage(e.getLocalizedMessage())
+                .setPositiveButton("确认", null)
+                .show();
+
+    }
+
+    //密码错误提示信息方法
+    void onFailure(Call call, String fail) {
+        new AlertDialog.Builder(this)
+                .setTitle("请求失败")
+                .setMessage(fail)
+                .setPositiveButton("确认", null)
+                .show();
+
+    }
+
+
     @Override
     protected void onResume() {
         super.onResume();
-
+//设置布局控件的text
         fragmentUsername.setLabelText("用户名：");
         fragmentUsername.setHintText("请输入用户名");
         fragmentPassword.setLabelText("密码：");
